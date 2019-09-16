6b88fcfd8080361442cb1eadc330a165fe03fbeb
Dedulko Nikita <36064675+DedulkoN@users.noreply.github.com>
2019-09-16 16:40:25 +0300

    #1

---
+++ MyBigNotebook/ClassCrypt.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace MyBigNotebook
+{
+  static  class ClassCrypt
+    {
+        /// <summary>
+        /// ключ
+        /// </summary>
+        static private string password = "JGT863_sdfkJKd2546s4+sf64sf4sefs+asef+fwf+wf5t6hdslg4535-sgert";
+
+        /// <summary>
+        /// Смена ключа шифрования
+        /// </summary>
+        /// <param name="NewKey"></param>
+        static public void SetKey(string NewKey)
+        {
+            password = NewKey;
+        }
+
+
+
+        static private byte[] Encrypt(byte[] data)
+        {
+            SymmetricAlgorithm sa = Rijndael.Create();
+            ICryptoTransform ct = sa.CreateEncryptor(
+                (new PasswordDeriveBytes(password, null)).GetBytes(16),
+                new byte[16]);
+
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, ct, CryptoStreamMode.Write);
+
+            cs.Write(data, 0, data.Length);
+            cs.FlushFinalBlock();
+
+            return ms.ToArray();
+        }
+
+        /// <summary>
+        /// Зашифровать строку
+        /// </summary>
+        /// <param name="data">Строка, которую необходимо зашифровать</param>
+        /// <returns>Зашифрованная строка</returns>
+        public static string Encrypt(string data)
+        {
+            return Convert.ToBase64String(Encrypt(Encoding.UTF8.GetBytes(data)));
+        }
+
+        private static byte[] Decrypt(byte[] data)
+        {
+            BinaryReader br = new BinaryReader(InternalDecrypt(data));
+            return br.ReadBytes(data.Length);
+        }
+
+        /// <summary>
+        /// Дешифрование
+        /// </summary>
+        /// <param name="data">Шифрованная строка</param>
+        /// <returns>Дешифрованная строка</returns>
+        public static string Decrypt(string data)
+        {
+            CryptoStream cs = InternalDecrypt(Convert.FromBase64String(data));
+            StreamReader sr = new StreamReader(cs);
+            return sr.ReadToEnd();
+        }
+
+        static CryptoStream InternalDecrypt(byte[] data)
+        {
+            SymmetricAlgorithm sa = Rijndael.Create();
+            ICryptoTransform ct = sa.CreateDecryptor(
+                (new PasswordDeriveBytes(password, null)).GetBytes(16),
+                new byte[16]);
+
+            MemoryStream ms = new MemoryStream(data);
+            return new CryptoStream(ms, ct, CryptoStreamMode.Read);
+        }
+    }
+
+    /// <summary>
+    /// Интерфейс для шифрования полей и классов
+    /// </summary>
+    interface ICrypt
+    {       
+        /// <summary>
+        /// Дешифрование
+        /// </summary>
+        /// <returns>True, если удачно</returns>
+        bool Decrypt();
+        /// <summary>
+        /// Шифрование
+        /// </summary>
+        /// <returns>True, если удачно</returns>
+        bool Encrypt();
+
+    }
+}
--- MyBigNotebook/ClassPlants.cs
+++ MyBigNotebook/ClassPlants.cs
@@ -6,10 +6,78 @@ using System.Threading.Tasks;
 
 namespace MyBigNotebook
 {
-    class ClassPlants
+    /// <summary>
+    /// Раздел планы
+    /// </summary>
+    [Serializable]
+    class ClassPlants:ICrypt
     {
+        /// <summary>
+        /// Планы на будущее
+        /// </summary>
+        List<FuturePlan> FuturePlans;
+        /// <summary>
+        /// Цели
+        /// </summary>
+        List<Target> Targets;
+        /// <summary>
+        /// Список закупок
+        /// </summary>
+        List<ShoppingList> ShoppingLists;
+        /// <summary>
+        /// Проекты
+        /// </summary>
+        List<Project> Projects;
 
+        ClassPlants()
+        {
+            FuturePlans = new List<FuturePlan>();
+            Targets = new List<Target>();
+            ShoppingLists = new List<ShoppingList>();
+            Projects = new List<Project>();
+        }
+        /// <summary>
+        /// Дешифрование
+        /// </summary>
+        /// <returns>Истина, если удачно</returns>
+        public bool Decrypt()
+        {
+            try
+            {
+                foreach (FuturePlan futurePlan in FuturePlans)
+                    futurePlan.Decrypt();
+                foreach (Target target in Targets)
+                    target.Decrypt();
+                foreach (ShoppingList shopping in ShoppingLists)
+                    shopping.Decrypt();
+                foreach (Project project in Projects)
+                    project.Decrypt();
 
+                return true;
+            }
+            catch { return false; }
+        }
+        /// <summary>
+        /// Шифрование
+        /// </summary>
+        /// <returns>Истина, если удачно</returns>
+        public bool Encrypt()
+        {
+            try
+            {
+                foreach (FuturePlan futurePlan in FuturePlans)
+                    futurePlan.Encrypt();
+                foreach (Target target in Targets)
+                    target.Encrypt();
+                foreach (ShoppingList shopping in ShoppingLists)
+                    shopping.Encrypt();
+                foreach (Project project in Projects)
+                    project.Encrypt();
+
+                return true;
+            }
+            catch { return false; }
+        }
 
     }
 
@@ -17,7 +85,7 @@ namespace MyBigNotebook
     /// Планы на будущее
     /// </summary>
     [Serializable]
-    class FuturePlan
+    class FuturePlan:ICrypt
     {
         /// <summary>
         /// Название плана
@@ -37,6 +105,32 @@ namespace MyBigNotebook
             NamePlan = "";
             DateRealise = DateTime.Now;
             punktOfPlans = new List<PunktOfPlan>();
+            
+        }
+
+        public bool Decrypt()
+        {
+            try
+            {
+                NamePlan = ClassCrypt.Decrypt(NamePlan);
+                foreach (PunktOfPlan punkt in punktOfPlans)
+                    punkt.Decrypt();
+
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool Encrypt()
+        {
+            try
+            {
+                NamePlan = ClassCrypt.Encrypt(NamePlan);
+                foreach (PunktOfPlan punkt in punktOfPlans)
+                    punkt.Encrypt();
+
+                return true;
+            }
+            catch { return false; }
         }
     }
 
@@ -44,7 +138,7 @@ namespace MyBigNotebook
     /// Пункт Плана
     /// </summary>
     [Serializable]
-    class PunktOfPlan
+    class PunktOfPlan:ICrypt
     {
         /// <summary>
         /// № пункта
@@ -65,13 +159,31 @@ namespace MyBigNotebook
             NamePunkt = "";
             CompleteFlag = false;
         }
+        public bool Encrypt()
+        {
+            try
+            {
+                NamePunkt = ClassCrypt.Encrypt(NamePunkt);
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool Decrypt()
+        {
+            try
+            {
+                NamePunkt = ClassCrypt.Decrypt(NamePunkt);
+                return true;
+            }
+            catch { return false; }
+        }
     }
 
     /// <summary>
     /// Шаг достижения цели
     /// </summary>
     [Serializable]
-    class Step
+    class Step:ICrypt
     {
         /// <summary>
         /// № шага
@@ -102,13 +214,33 @@ namespace MyBigNotebook
             DurationStep = DateTime.Now;
             CompleteFlag = false;
         }
+        public bool Decrypt()
+        {
+            try
+            {
+                NameStep = ClassCrypt.Decrypt(NameStep);
+                DescriptionStep = ClassCrypt.Decrypt(DescriptionStep);
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool Encrypt()
+        {
+            try
+            {
+                NameStep = ClassCrypt.Encrypt(NameStep);
+                DescriptionStep = ClassCrypt.Encrypt(DescriptionStep);
+                return true;
+            }
+            catch { return false; }
+        }
     }
 
     /// <summary>
     /// Цель
     /// </summary>
     [Serializable]
-    class Target
+    class Target:ICrypt
     {
         /// <summary>
         /// название
@@ -144,9 +276,188 @@ namespace MyBigNotebook
             Steps = new List<Step>();
             CompleteFlag = false;
         }
+        public bool Decrypt()
+        {
+            try
+            {
+                NameTarget = ClassCrypt.Decrypt(NameTarget);
+                DescriptionTarget = ClassCrypt.Decrypt(DescriptionTarget);
+                foreach (Step step in Steps)
+                    step.Decrypt();
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool Encrypt()
+        {
+            try
+            {
+                NameTarget = ClassCrypt.Encrypt(NameTarget);
+                DescriptionTarget = ClassCrypt.Encrypt(DescriptionTarget);
+                foreach (Step step in Steps)
+                    step.Encrypt();
+                return true;
+            }
+            catch { return false; }
+        }
     }
 
-    
+    /// <summary>
+    /// Список покупок
+    /// </summary>
+    [Serializable]
+    class ShoppingList:ICrypt
+    {
+        /// <summary>
+        /// Название
+        /// </summary>
+        string NameItem;
+        /// <summary>
+        /// Описание
+        /// </summary>
+        string Description;
+        /// <summary>
+        ///Цена
+        /// </summary>
+        double Cost;
+        /// <summary>
+        /// Отметка о покупке
+        /// </summary>
+        bool CompleteFlag;
+
+        ShoppingList()
+        {
+            NameItem = "";
+            Description = "";
+            Cost = 0;
+            CompleteFlag = false;
+        }
+        public bool Decrypt()
+        {
+            try
+            {
+                NameItem = ClassCrypt.Decrypt(NameItem);
+                Description = ClassCrypt.Decrypt(Description);
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool Encrypt()
+        {
+            try
+            {
+                NameItem = ClassCrypt.Encrypt(NameItem);
+                Description = ClassCrypt.Encrypt(Description);
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+
+    /// <summary>
+    /// Проекты
+    /// </summary>
+    [Serializable]
+    class Project:ICrypt
+    {
+        /// <summary>
+        /// Название проекта
+        /// </summary>
+        string NameProject;
+        /// <summary>
+        /// Описание
+        /// </summary>
+        string Description;
+        /// <summary>
+        /// Дата начала
+        /// </summary>
+        DateTime DateStart;
+        /// <summary>
+        /// Шаги реализации
+        /// </summary>
+        List<ProjectStep> ProjectSteps;
+
+        Project()
+        {
+            NameProject = "";
+            Description = "";
+            DateStart = DateTime.Now;
+            ProjectSteps = new List<ProjectStep>();
+        }
+        public bool Decrypt()
+        {
+            try
+            {
+                NameProject = ClassCrypt.Decrypt(NameProject);
+                Description = ClassCrypt.Decrypt(Description);
+                foreach (ProjectStep step in ProjectSteps)
+                    step.Decrypt();
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool Encrypt()
+        {
+            try
+            {
+                NameProject = ClassCrypt.Encrypt(NameProject);
+                Description = ClassCrypt.Encrypt(Description);
+                foreach (ProjectStep step in ProjectSteps)
+                    step.Encrypt();
+                return true;
+            }
+            catch { return false; }
+        }
+
+    }
+
+    [Serializable]
+    /// <summary>
+    /// пункт реализации проекта
+    /// </summary>
+    class ProjectStep:ICrypt
+    {
+        /// <summary>
+        /// наименование
+        /// </summary>
+        string NameStep;
+        /// <summary>
+        /// Описание
+        /// </summary>
+        string Description;
+        /// <summary>
+        /// Отметка о выполнении
+        /// </summary>
+        bool CombleteFlag;
+
+        ProjectStep()
+        {
+            NameStep = "";
+            Description = "";
+            CombleteFlag = false;
+        }
+        public bool Decrypt()
+        {
+            try
+            {
+                NameStep = ClassCrypt.Decrypt(NameStep);
+                Description = ClassCrypt.Decrypt(Description);
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool Encrypt()
+        {
+            try
+            {
+                NameStep = ClassCrypt.Encrypt(NameStep);
+                Description = ClassCrypt.Encrypt(Description);
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+
 
 
 }
